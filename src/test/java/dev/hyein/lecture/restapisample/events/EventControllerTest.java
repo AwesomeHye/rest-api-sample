db2b12fdcec4e69ec36b3b5bdc5ec2df49709381
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2020-04-03 12:41:15 +0900

    add test description annotation

--- src/test/java/dev/hyein/lecture/restapisample/events/EventControllerTest.java
+++ src/test/java/dev/hyein/lecture/restapisample/events/EventControllerTest.java
@@ -1,6 +1,7 @@
 package dev.hyein.lecture.restapisample.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.hyein.lecture.restapisample.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,6 +37,7 @@ public class EventControllerTest {
 
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -71,6 +73,7 @@ public class EventControllerTest {
      * @throws Exception
      */
     @Test
+    @TestDescription("입력받을 수 없는 값을 입력받는 경우 에러 발생하는 테스트")
     public void createEvent_bad_request_unknown_input() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -102,6 +105,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력받이 빈 경우 에러 발생하는 테스트")
     public void createEvent_bad_request_empty_input() throws Exception{
         EventDto eventDto = EventDto.builder().build();
 
@@ -114,6 +118,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력값이 틀린 경우 에러 발생하는 테스트")
     public void createEvent_bad_request_wrong_input() throws Exception{
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
