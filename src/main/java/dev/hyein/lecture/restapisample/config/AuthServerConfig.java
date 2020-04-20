64cceb5b600f6f2b5df9199a3b0e0f8add94b6f7
seohyein <hyein06@datasolution.kr>
2020-04-20 22:30:19 +0900

    add oauth2 token server

---
+++ src/main/java/dev/hyein/lecture/restapisample/config/AuthServerConfig.java
@@ -0,0 +1,65 @@
+package dev.hyein.lecture.restapisample.config;
+
+        import dev.hyein.lecture.restapisample.account.AccountService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.authentication.AuthenticationManager;
+        import org.springframework.security.crypto.password.PasswordEncoder;
+        import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+        import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+        import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+        import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+        import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+        import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
+    // for configure(AuthorizationServerEndpointsConfigurer)
+    @Autowired
+    AuthenticationManager authenticationManager; // 유저 인증 정보를 가지고 있음
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    TokenStore tokenStore;
+
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // client_secret을 인코딩할 password encoder 설정
+        security.passwordEncoder(passwordEncoder);
+    }
+
+    /**
+     * 서버에게 토큰을 요청할 클라이언트 설정
+     * @param clients
+     * @throws Exception
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("CLIENT_ID")
+                .secret(passwordEncoder.encode("CLIENT_SECRET"))
+                .authorizedGrantTypes("password", "refresh_token") // 이 인증 서버가 지원할 토큰 설정. refresh_token: auth token받을 때 발급받는 토큰으로 새로운 access token을 발급받는다.
+                .scopes("read", "write")
+                .accessTokenValiditySeconds(60 * 10)
+                .refreshTokenValiditySeconds(60 * 60);
+    }
+
+
+    /**
+     * 유저 인증 서버 (이전에 만들어둔 커스터마이징 인증 서버 적용)
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(accountService)
+                .tokenStore(tokenStore);
+    }
+}
---
+++ src/test/java/dev/hyein/lecture/restapisample/config/AuthServerConfigTest.java
@@ -0,0 +1,56 @@
+package dev.hyein.lecture.restapisample.config;
+
+import dev.hyein.lecture.restapisample.account.Account;
+import dev.hyein.lecture.restapisample.account.AccountRepository;
+import dev.hyein.lecture.restapisample.account.AccountRole;
+import dev.hyein.lecture.restapisample.account.AccountService;
+import dev.hyein.lecture.restapisample.common.BaseControllerTest;
+import dev.hyein.lecture.restapisample.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class AuthServerConfigTest extends BaseControllerTest {
+    @Autowired
+    AccountService accountService;
+
+
+    @Test
+    @TestDescription("인증 토큰을 발급받는 테스트")
+    public void getAuthToken() throws Exception{
+        //given
+        String userName = "catsarah@gmail.com";
+        String password = "cat";
+        Account account = Account.builder()
+                                .email(userName)
+                                .password(password)
+                                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                                .build();
+        accountService.saveAccount(account);
+
+
+        // then
+        String clientId = "CLIENT_ID";
+        String clientSecret = "CLIENT_SECRET";
+        mockMvc.perform(post("/oauth/token")
+                        .with(httpBasic(clientId, clientSecret)) // http basic 인증 사용
+                        .param("grant_type", "password")
+                        .param("username", userName)
+                        .param("password", password))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists())
+                .andExpect(jsonPath("token_type").exists())
+                .andExpect(jsonPath("expires_in").exists())
+                .andExpect(jsonPath("scope").exists());
+    }
+}
